7201e140aa5fd248e392f467906d41ebae6356a3
Selwyn Hanselman <s.r.hanselman@gmail.com>
2015-02-12 17:29:15 +0100

    This version includes a force calculator including automatic correlation function updates (in PhysicalProperties.f90).

--- src/ForceCalculator.f90
+++ src/ForceCalculator.f90
@@ -2,6 +2,15 @@
 module ForceCalculator
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+! COMMENTS 02/12/2015
+!	In calculating the correlation function, it may be convenient to store the r_{ij}^2
+!	and simply reuse this array in the correlation function. However, this requires a major
+!	rewrite of the force_(potential_)calculator subroutine, involving a second particleDistanceSq
+!	array. I'll do just that, but in case we find a more efficient way of coding, we might
+!	revert to the previous commit.
+
+use PhysicalProperties
+
 implicit none
 private
 
@@ -9,11 +18,10 @@ public force_calculator
 public force_potential_calculator
 
 
-
 contains
 
 
-subroutine force_calculator(rm,bondingEnergy,maxForceDistance,L,x,f)
+subroutine force_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,particleDistanceSq)
 	! One generally would not need to directly call this subroutine.
 	! rm is the potential minimum distance, bondingEnergy is the interaction depth,
 	! L is the box length, x contains the particle positions while f contains the
@@ -21,16 +29,19 @@ subroutine force_calculator(rm,bondingEnergy,maxForceDistance,L,x,f)
 
 	integer ::		  i, j, N
 	real*8, intent(in) :: 	  rm, bondingEnergy, x(:,:), L, maxForceDistance
-	real*8, intent(out) ::    f(:,:)
+	real*8, intent(out) ::    f(:,:), particleDistanceSq(size(x,1),size(x,1))
 	real*8 ::		  dx(3), distancesq, fPair(3)
 
 	N = size(x,1)
 	f = 0
+	particleDistanceSq = 0
+
 	do i=1,N
 		do j=i+1,N
 			dx = x(i,:) - x(j,:)
 			dx = dx-nint(dx/L)*L
 			distancesq = dot_product(dx,dx)
+			particleDistanceSq(i,j) = distancesq
 			if (distancesq < maxForceDistance*maxForceDistance) then
 				distancesq = 1/distancesq
 				fPair =  12*bondingEnergy*dx*(rm**6*distancesq**4-rm**12*distancesq**7)
@@ -44,22 +55,24 @@ subroutine force_calculator(rm,bondingEnergy,maxForceDistance,L,x,f)
 end subroutine
 
 
-subroutine force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,potential)
+subroutine force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,potential,particleDistanceSq)
 
 	real*8, intent(in) :: 	  rm, bondingEnergy, x(:,:), L, maxForceDistance
 	real*8, intent(inout) ::  f(:,:)
-	real*8, intent(out) ::    potential(size(x,1))
+	real*8, intent(out) ::    potential(size(x,1)), particleDistanceSq(size(x,1),size(x,1))
 	real*8 ::		  dx(3), distancesq
 	real*8 ::                 fPair(3), potentialPair
 	integer ::		  i, j, N
 	
 	N = size(x,1)
+	particleDistanceSq = 0
 
 	do i=1,N
 		do j=i+1,N
 			dx = x(i,:) - x(j,:)
 			dx = dx-nint(dx/L)*L
 			distancesq = dot_product(dx,dx)
+			particleDistanceSq(i,j) = distancesq
 			if (distancesq < maxForceDistance*maxForceDistance) then
 				distancesq = 1/distancesq
 				fPair =  12*bondingEnergy*dx*(rm**6*distancesq**4-rm**12*distancesq**7)
@@ -75,6 +88,77 @@ subroutine force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,po
 
 end subroutine
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!FUNCTIONS AFTER THIS LINE INCLUDE HISTOGRAM CREATION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine force_calculator_correlation(rm,bondingEnergy,maxForceDistance,L,x,f,dR,correlation)
+	! One generally would not need to directly call this subroutine.
+	! rm is the potential minimum distance, bondingEnergy is the interaction depth,
+	! L is the box length, x contains the particle positions while f contains the
+	! force output.
+
+	integer ::		  i, j, N
+	real*8, intent(in) :: 	  rm, bondingEnergy, x(:,:), L, maxForceDistance, dR
+	real*8, intent(inout) ::  f(:,:)
+	real*8, intent(out) ::    correlation(ceiling(sqrt(3.)*L/(2*dR))+1,2)
+	real*8 ::		  dx(3), distancesq, fPair(3)
+
+	N = size(x,1)
+	f = 0
+	correlation = 0
+
+	do i=1,N
+		do j=i+1,N
+			dx = x(i,:) - x(j,:)
+			dx = dx-nint(dx/L)*L
+			distancesq = dot_product(dx,dx)
+			call correlation_function_scalar(distancesq,dR,L,correlation)
+			if (distancesq < maxForceDistance*maxForceDistance) then
+				distancesq = 1/distancesq
+				fPair =  12*bondingEnergy*dx*(rm**6*distancesq**4-rm**12*distancesq**7)
+				f(i,:) = f(i,:) + fPair
+				f(j,:) = f(j,:) - fPair
+			end if
+		end do
+	end do
+
+
+end subroutine
+
+
+subroutine force_potential_calculator_correlation(rm,bondingEnergy,maxForceDistance,L,x,f,potential,dR,correlation)
+
+	real*8, intent(in) :: 	  rm, bondingEnergy, x(:,:), L, maxForceDistance, dR
+	real*8, intent(inout) ::  f(:,:)
+	real*8, intent(out) ::    potential(size(x,1)), correlation(ceiling(sqrt(3.)*L/(2*dr))+1,2)
+	real*8 ::		  dx(3), distancesq
+	real*8 ::                 fPair(3), potentialPair
+	integer ::		  i, j, N
+	
+	N = size(x,1)
+	correlation = 0
+
+	do i=1,N
+		do j=i+1,N
+			dx = x(i,:) - x(j,:)
+			dx = dx-nint(dx/L)*L
+			distancesq = dot_product(dx,dx)
+			call correlation_function_scalar(distancesq,dR,L,correlation)
+			if (distancesq < maxForceDistance*maxForceDistance) then
+				distancesq = 1/distancesq
+				fPair =  12*bondingEnergy*dx*(rm**6*distancesq**4-rm**12*distancesq**7)
+				potentialPair = bondingEnergy*(2*rm**6*distancesq**3-rm**12*distancesq**6)
+				f(i,:) = f(i,:) + fPair
+				f(j,:) = f(j,:) - fPair
+				potential(i) = potential(i) + potentialPair
+				potential(j) = potential(j) + potentialPair
+			end if
+		end do
+	end do
+
+
+end subroutine
 
 
 
---
+++ src/PhysicalProperties.f90
@@ -0,0 +1,102 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+module PhysicalProperties
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+implicit none
+private
+
+public calculate_temperature                         ! This subroutine calculates the temperature.
+public calculate_pressure                            ! A stub as of 12/02/2015
+public correlation_function_scalar		     ! This one compiles, as opposed to the external array one;
+						! this subroutine will be called by ForceCalculator.
+
+contains
+
+
+subroutine calculate_temperature (particleKineticEnergy,meanMomentumSq,mass,temperatureCalc)
+	! This subroutine uses the equipartition theorem (in which (<p^2>-<p>^2)/2m = 3kB*T/2)
+	! to calculate the temperature (T = (<p^2>-<p>^2)/(3kB*m)) = Ekin(2/3kB)-<p>^2/3kB*m.
+	real*8, intent(in) ::   particleKineticEnergy(:),meanMomentumSq, mass
+	real*8, intent(out) ::  temperatureCalc
+	real*8 ::               kB
+
+	kB = 1.3806488d-23
+	
+	temperatureCalc = 2*sum(particleKineticEnergy) - meanMomentumSq/mass
+	temperatureCalc = temperatureCalc/(3*kB*size(particleKineticEnergy))
+end subroutine
+
+subroutine calculate_pressure (x,f,L,pressure)
+	! This subroutine uses the virial theorem to calculate the pressure of the multi-particle model.
+	! PV = N*kB*T(1 - f(x,F)/3N)
+	real*8, intent(in)  :: x(:,:), f(:,:), L
+	real*8, intent(out) :: pressure
+
+end subroutine
+
+subroutine correlation_function_scalar (particleDistanceSq,dr,L,g)
+	! This subroutine divides one single relative distance between by its spherical shell depth,
+	! adding it to histogram g(#,r;dr).
+
+	real*8, intent(in)  :: dr, L, particleDistanceSq
+	real*8, intent(inout) :: g(:)
+	real*8              :: pi
+	integer             :: i
+
+	pi = atan2(0d0,-1d0)
+
+	! The histogram is based on spherical shells centred -around- g(1,:).
+	! Hence, their corresponding volumes are 4pi/3((r+dr/2)**3-(r-dr/2)**3) = 4pi/3(3r**2*dr+(dr**3)/4)
+	! or dV = (4*r**2+dr**2/3)*dr*pi = (4pi*i**2*dr**3 +dr**2/3)*dr = (4*i**2 + 1/3)*pi*dr**3
+
+	i = nint(sqrt(particleDistanceSq)/dr)
+	g(i) = g(i) + (4.*i**2 + 1./3)*pi*dr**3
+
+end subroutine
+
+
+
+
+!subroutine correlation_function_array (particleDistanceSq,dr,L,g)
+	! This subroutine divides all relative distances between by their spherical shell depths,
+	! plotting them to histogram g(#,r;dr).
+	! Note that the particle distance matrix includes N(N-1)/2 valid values; all others (N(N+1)/2) are zeroes
+	! and should be subtracted from the correlation histogram.
+
+!	real*8, intent(in)  :: particleDistanceSq(:,:), dr, L
+!	real*8, intent(out) :: g(ceiling(sqrt(3.)*L/(2*dr))+1,2)
+!	real*8              :: pi
+!	integer             :: i, N, particleDistances(size(particleDistanceSq,1)*size(particleDistanceSq,1))
+
+!	pi = atan2(0d0,-1d0)
+!	N = size(particleDistanceSq,1)	
+!	particleDistances = nint(sqrt(reshape(particleDistanceSq,N*N))))
+
+!	do i=1,size(g,1)
+!		g(i,1) = dr*i
+!		g(i,2) = 0
+!	end do
+!
+!	do i=1,N
+!		g(particleDistances(i),2) = g(particleDistances(i),2)+1
+!	end do
+!	
+!	g(1,2) = g(1,2) - N*(N+1)/2
+!
+!	! The histogram is based on spherical shells centred -around- g(1,:).
+!	! Hence, their corresponding volumes are 4pi/3((r+dr/2)**3-(r-dr/2)**3) = 4pi/3(3r**2*dr+(dr**3)/4)
+!	! or dV = (4*r**2+dr**2/3)*dr*pi = (4pi*i**2*dr**3 +dr**2/3)*dr = (4*i**2 + 1/3)*pi*dr**3
+!
+!	do i=1,N
+!		g(particleDistances(i),2) = g(particleDistances(i),2)/(4.*particleDistances(i)**2+1/3.)
+!		g(particleDistances(i),2) = g(particleDistances(i),2)/(pi*dr*dr*dr)
+!	end do
+!
+!end subroutine
+	
+ 
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+end module PhysicalProperties
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
