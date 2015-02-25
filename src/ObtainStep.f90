8e88f4acf271bc39b0d0862715e4911cf352dc67
Selwyn Hanselman <s.r.hanselman@gmail.com>
2015-02-25 11:53:41 +0100

    This does not work for some reason; the potential vector is allocated to an extreme amount of memory.

--- src/ObtainStep.f90
+++ src/ObtainStep.f90
@@ -15,13 +15,14 @@ private
 
 public obtain_step                                   ! Call this subroutine when energy is presumed to be stable.
 public obtain_step_automatic_checks                  ! A lot slower, but automatically sets warning flags for clear transgressions.
-
+public obtain_step_correlation			     ! Integrates correlation function calculation.
 
 
 contains
 
 subroutine obtain_step(x,p,f,L,maxForceDistance,rm,bondingEnergy,mass,timeStep,meanMomentumSq, &
- particleKineticEnergy,particlePotential,kineticEnergyAfterStep,potentialEnergyAfterStep)
+ particleKineticEnergy,particlePotential,kineticEnergyAfterStep,potentialEnergyAfterStep, &
+ particleDistanceSq)
 	! This function uses a simple velocity Verlet algorithm.
 	! Its arguments are as listed above; note that x, p, f are position, momentum
 	!    and force respectively, and L is the box length. outOfVelocityBounds 
@@ -34,14 +35,16 @@ subroutine obtain_step(x,p,f,L,maxForceDistance,rm,bondingEnergy,mass,timeStep,m
 	real*8, intent(in) ::    mass, timeStep, rm, bondingEnergy, L, maxForceDistance ! maxBeta is dimensionless, mass in kg, L in m, timeStep in s, momentumTolerance is dimensionless (# stddevs)
 	real*8, intent(inout) :: x(:,:), p(:,:), f(:,:)                              ! position, momentum and force vectors
 	real*8, intent(out) ::   kineticEnergyAfterStep, potentialEnergyAfterStep, &
- particleKineticEnergy(size(x,1)), particlePotential(size(x,1))
+ particleKineticEnergy(size(x,1)), particlePotential(size(x,1)), &
+ particleDistanceSq(size(x,1),size(x,1))
 		! these flags signal clear divergences in individual and mean momentum
 	real*8 ::                meanMomentumSq
 
 
         p = p + f*timeStep/2
 	x = modulo(x+p*timeStep/mass,L)
-	call force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,particlePotential)
+	call force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,particlePotential, &
+	 particleDistanceSq)
 	p = p + f * timeStep/(2*mass)
 	
 
@@ -49,9 +52,6 @@ subroutine obtain_step(x,p,f,L,maxForceDistance,rm,bondingEnergy,mass,timeStep,m
 	particleKineticEnergy = dot_product(p(i,:),p(i,:))/(2*mass)
 	kineticEnergyAfterStep = sum(particleKineticEnergy)
 	potentialEnergyAfterStep = sum(particlePotential)
-
-
-
 
 end subroutine
 
@@ -61,7 +61,7 @@ end subroutine
 
 subroutine obtain_step_automatic_checks(x,p,f,L,maxBeta,momentumTolerance,mass,timeStep,outOfVelocityBounds, &
  kineticEnergyAfterStep,particleMomentumSq,initialKineticEnergy,particlePotential,potentialEnergyAfterStep, &
- energyTolerance,meanMomentumDiverged,rm,bondingEnergy,energyNotPreserved,maxForceDistance)
+ energyTolerance,meanMomentumDiverged,rm,bondingEnergy,energyNotPreserved,maxForceDistance, particleDistanceSq)
 
 	! To automatically check for issues, use the flags displayed under logical, intent(out).
 
@@ -71,7 +71,7 @@ subroutine obtain_step_automatic_checks(x,p,f,L,maxBeta,momentumTolerance,mass,t
            ! maxBeta is dimensionless, mass in kg, timeStep in s, momentumTolerance/energyTolerance are dimensionless (# stddevs)
 	real*8, intent(inout) :: x(:,:), p(:,:), f(:,:)                              ! position, momentum and force vectors
 	real*8, intent(out) ::   kineticEnergyAfterStep, particlePotential(size(x,1)), potentialEnergyAfterStep, &
- particleMomentumSq(size(x,1))
+ particleMomentumSq(size(x,1)),  particleDistanceSq(size(x,1),size(x,1))
 	logical, intent(out) ::   outOfVelocityBounds(size(x,1)), meanMomentumDiverged, energyNotPreserved ! these flags signal clear divergences in individual and mean momentum
 				
 	real*8 ::  lightSpeed, temperature, stddevsq, kB, maxMomentum
@@ -99,7 +99,7 @@ subroutine obtain_step_automatic_checks(x,p,f,L,maxBeta,momentumTolerance,mass,t
 	end do
 
 
-	call force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,particlePotential)
+	call force_potential_calculator(rm,bondingEnergy,maxForceDistance,L,x,f,particlePotential,particleDistanceSq)
 	p = p + f * timeStep/2
 	
 
@@ -115,7 +115,42 @@ subroutine obtain_step_automatic_checks(x,p,f,L,maxBeta,momentumTolerance,mass,t
 
 end subroutine
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+subroutine obtain_step_correlation(x,p,f,L,maxForceDistance,rm,bondingEnergy,mass,timeStep,meanMomentumSq, &
+ particleKineticEnergy,particlePotential,kineticEnergyAfterStep,potentialEnergyAfterStep,dR,correlation)
+	! This function uses a simple velocity Verlet algorithm.
+	! Its arguments are as listed above; note that x, p, f are position, momentum
+	!    and force respectively, and L is the box length. outOfVelocityBounds 
+	!    indicates whether the program should warn/exit due to exceeding the initially
+	!    defined velocities, whereas meanMomentumDiverged does the same for a
+	!    out-of-bounds shift of mean momentum. rm indicates the individual interaction
+	!    potential minimum while bondingEnergy indicates the potential depth.
+
+	integer :: i                                                                  ! a plain old iterator
+	real*8, intent(in) ::    mass, timeStep, rm, bondingEnergy, L, maxForceDistance, dR
+             ! maxBeta is dimensionless, mass in kg, L in m, timeStep in s, momentumTolerance is dimensionless (# stddevs)
+	real*8, intent(inout) :: x(:,:), p(:,:), f(:,:), correlation(:), particleKineticEnergy(size(x,1)), &
+ particlePotential(size(x,1))           ! position, momentum and force vectors
+	real*8, intent(out) ::   kineticEnergyAfterStep, potentialEnergyAfterStep
+		! these flags signal clear divergences in individual and mean momentum
+	real*8 ::                meanMomentumSq
+
+
+        p = p + f*timeStep/2
+	x = modulo(x+p*timeStep/mass,L)
+	call force_calculator_correlation(rm,bondingEnergy,maxForceDistance,L,x,f,dR,correlation)
+	p = p + f * timeStep/(2*mass)
+	
+
+	meanMomentumSq = dot_product(sum(p(:,:),1),sum(p(:,:),1))                     ! sums over all particles BEFORE obtaining the modulus squared - see Changelog 2/4/2015
+	particleKineticEnergy = dot_product(p(i,:),p(i,:))/(2*mass)
+	kineticEnergyAfterStep = sum(particleKineticEnergy)
+	potentialEnergyAfterStep = sum(particlePotential)
+
+end subroutine
 
 
 
